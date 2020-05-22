1f8318d251224fa3cfaba27b1beb12d44277157e
Christian Deacon <gamemann@gflclan.com>
2020-05-22 14:20:16 +0000

    Cleaned up code.

--- src/afxdp_user.c
+++ src/afxdp_user.c
@@ -24,35 +24,36 @@
 #include "../libbpf/src/libbpf.h"
 #include "../libbpf/src/xsk.h"
 
-#define NUM_FRAMES         4096
-#define FRAME_SIZE         XSK_UMEM__DEFAULT_FRAME_SIZE
-#define RX_BATCH_SIZE      64
+#define NUM_FRAMES 4096
+#define FRAME_SIZE XSK_UMEM__DEFAULT_FRAME_SIZE
+#define RX_BATCH_SIZE 64
 #define INVALID_UMEM_FRAME UINT64_MAX
 
 struct xsk_umem_info 
 {
-	struct xsk_ring_prod fq;
-	struct xsk_ring_cons cq;
-	struct xsk_umem *umem;
-	void *buffer;
+    struct xsk_ring_prod fq;
+    struct xsk_ring_cons cq;
+    struct xsk_umem *umem;
+    void *buffer;
 };
 
 struct xsk_socket_info
 {
-	struct xsk_ring_cons rx;
-	struct xsk_ring_prod tx;
-	struct xsk_umem_info *umem;
-	struct xsk_socket *xsk;
+    struct xsk_ring_cons rx;
+    struct xsk_ring_prod tx;
+    struct xsk_umem_info *umem;
+    struct xsk_socket *xsk;
 
-	uint64_t umem_frame_addr[NUM_FRAMES];
-	uint32_t umem_frame_free;
+    uint64_t umem_frame_addr[NUM_FRAMES];
+    uint32_t umem_frame_free;
 
-	uint32_t outstanding_tx;
+    uint32_t outstanding_tx;
 };
 
 static int cont = 1;
 const char *dev = "ens3";
 static int progfd;
+uint32_t xdp_flags = XDP_FLAGS_DRV_MODE;
 
 void ShutDown(int tmp)
 {
@@ -61,111 +62,111 @@ void ShutDown(int tmp)
 
 static inline __u32 xsk_ring_prod__free(struct xsk_ring_prod *r)
 {
-	r->cached_cons = *r->consumer + r->size;
-	return r->cached_cons - r->cached_prod;
-}
+    r->cached_cons = *r->consumer + r->size;
+    return r->cached_cons - r->cached_prod;
+    }
 
 static struct xsk_umem_info *configure_xsk_umem(void *buffer, uint64_t size)
 {
-	struct xsk_umem_info *umem;
-	int ret;
+    struct xsk_umem_info *umem;
+    int ret;
 
-	umem = calloc(1, sizeof(*umem));
-	if (!umem)
+    umem = calloc(1, sizeof(*umem));
+    if (!umem)
     {
-		return NULL;
+        return NULL;
     }
 
-	ret = xsk_umem__create(&umem->umem, buffer, size, &umem->fq, &umem->cq,
-			       NULL);
-	if (ret) {
-		errno = -ret;
-		return NULL;
-	}
+    ret = xsk_umem__create(&umem->umem, buffer, size, &umem->fq, &umem->cq,
+                    NULL);
+    if (ret) {
+        errno = -ret;
+        return NULL;
+    }
 
-	umem->buffer = buffer;
-	return umem;
+    umem->buffer = buffer;
+    return umem;
 }
 
 static uint64_t xsk_alloc_umem_frame(struct xsk_socket_info *xsk)
 {
-	uint64_t frame;
-	if (xsk->umem_frame_free == 0)
-		return INVALID_UMEM_FRAME;
+    uint64_t frame;
+    if (xsk->umem_frame_free == 0)
+        return INVALID_UMEM_FRAME;
 
-	frame = xsk->umem_frame_addr[--xsk->umem_frame_free];
-	xsk->umem_frame_addr[xsk->umem_frame_free] = INVALID_UMEM_FRAME;
-	return frame;
+    frame = xsk->umem_frame_addr[--xsk->umem_frame_free];
+    xsk->umem_frame_addr[xsk->umem_frame_free] = INVALID_UMEM_FRAME;
+    return frame;
 }
 
 static void xsk_free_umem_frame(struct xsk_socket_info *xsk, uint64_t frame)
 {
-	assert(xsk->umem_frame_free < NUM_FRAMES);
+    assert(xsk->umem_frame_free < NUM_FRAMES);
 
-	xsk->umem_frame_addr[xsk->umem_frame_free++] = frame;
+    xsk->umem_frame_addr[xsk->umem_frame_free++] = frame;
 }
 
 static uint64_t xsk_umem_free_frames(struct xsk_socket_info *xsk)
 {
-	return xsk->umem_frame_free;
+    return xsk->umem_frame_free;
 }
 
 static struct xsk_socket_info *xsk_configure_socket(struct xsk_umem_info *umem, int rxqueue, int ifidx)
 {
-	struct xsk_socket_config xsk_cfg;
-	struct xsk_socket_info *xsk_info;
-	uint32_t idx;
-	uint32_t prog_id = 0;
-	int i;
-	int ret;
+    struct xsk_socket_config xsk_cfg;
+    struct xsk_socket_info *xsk_info;
+    uint32_t idx;
+    uint32_t prog_id = 0;
+    int i;
+    int ret;
 
-	xsk_info = calloc(1, sizeof(*xsk_info));
-	if (!xsk_info)
-		return NULL;
+    xsk_info = calloc(1, sizeof(*xsk_info));
+    if (!xsk_info)
+        return NULL;
 
-	xsk_info->umem = umem;
-	xsk_cfg.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS;
-	xsk_cfg.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;
-	xsk_cfg.libbpf_flags = 0;
-	xsk_cfg.xdp_flags = XDP_FLAGS_DRV_MODE;
-	xsk_cfg.bind_flags = XDP_COPY;
-	ret = xsk_socket__create(&xsk_info->xsk, dev, rxqueue, umem->umem, &xsk_info->rx, &xsk_info->tx, &xsk_cfg);
+    xsk_info->umem = umem;
+    xsk_cfg.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS;
+    xsk_cfg.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;
+    xsk_cfg.libbpf_flags = 0;
+    xsk_cfg.xdp_flags = xdp_flags;
+    xsk_cfg.bind_flags = XDP_COPY;
+    ret = xsk_socket__create(&xsk_info->xsk, dev, rxqueue, umem->umem, &xsk_info->rx, &xsk_info->tx, &xsk_cfg);
 
-	if (ret)
-		goto error_exit;
+    if (ret)
+        goto error_exit;
 
-	ret = bpf_get_link_xdp_id(ifidx, &prog_id, XDP_FLAGS_DRV_MODE);
+    ret = bpf_get_link_xdp_id(ifidx, &prog_id, XDP_FLAGS_DRV_MODE);
 
-	if (ret)
-		goto error_exit;
+    if (ret)
+        goto error_exit;
 
-	/* Initialize umem frame allocation */
+    /* Initialize umem frame allocation */
 
-	for (i = 0; i < NUM_FRAMES; i++)
-		xsk_info->umem_frame_addr[i] = i * FRAME_SIZE;
+    for (i = 0; i < NUM_FRAMES; i++)
+        xsk_info->umem_frame_addr[i] = i * FRAME_SIZE;
 
-	xsk_info->umem_frame_free = NUM_FRAMES;
+    xsk_info->umem_frame_free = NUM_FRAMES;
 
-	/* Stuff the receive path with buffers, we assume we have enough */
-	ret = xsk_ring_prod__reserve(&xsk_info->umem->fq,
-				     XSK_RING_PROD__DEFAULT_NUM_DESCS,
-				     &idx);
+    /* Stuff the receive path with buffers, we assume we have enough */
+    ret = xsk_ring_prod__reserve(&xsk_info->umem->fq,
+                        XSK_RING_PROD__DEFAULT_NUM_DESCS,
+                        &idx);
 
-	if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
-		goto error_exit;
+    if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
+        goto error_exit;
 
-	for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i ++)
-		*xsk_ring_prod__fill_addr(&xsk_info->umem->fq, idx++) =
-			xsk_alloc_umem_frame(xsk_info);
+    for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i ++)
+        *xsk_ring_prod__fill_addr(&xsk_info->umem->fq, idx++) =
+            xsk_alloc_umem_frame(xsk_info);
 
-	xsk_ring_prod__submit(&xsk_info->umem->fq,
-			      XSK_RING_PROD__DEFAULT_NUM_DESCS);
+    xsk_ring_prod__submit(&xsk_info->umem->fq,
+                    XSK_RING_PROD__DEFAULT_NUM_DESCS);
 
-	return xsk_info;
+    return xsk_info;
 
 error_exit:
-	errno = -ret;
-	return NULL;
+    errno = -ret;
+    return NULL;
 }
 
 int find_map_fd(struct bpf_object *bpf_obj, const char *mapname)
@@ -279,12 +280,12 @@ void *PollXSK(void *data)
 {
     struct xsk_socket_info *xsk = (struct xsk_socket_info *)data;
 
-	struct pollfd fds[2];
-	int ret, nfds = 1;
+    struct pollfd fds[2];
+    int ret, nfds = 1;
 
-	memset(fds, 0, sizeof(fds));
-	fds[0].fd = xsk_socket__fd(xsk->xsk);
-	fds[0].events = POLLIN;
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = xsk_socket__fd(xsk->xsk);
+    fds[0].events = POLLIN;
 
     while (cont)
     {
@@ -310,13 +311,13 @@ void *PollXSK(void *data)
 
 int main(int argc, char **argv)
 {
-	int ret;
+    int ret;
     int ifidx;
-	int xsks_map_fd;
-	void *packet_buffer;
-	uint64_t packet_buffer_size;
-	struct rlimit rlim = {RLIM_INFINITY, RLIM_INFINITY};
-	struct bpf_object *bpf_obj = NULL;
+    int xsks_map_fd;
+    void *packet_buffer;
+    uint64_t packet_buffer_size;
+    struct rlimit rlim = {RLIM_INFINITY, RLIM_INFINITY};
+    struct bpf_object *bpf_obj = NULL;
 
     ifidx = if_nametoindex(dev);
 
@@ -327,12 +328,12 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-	/* Global shutdown handler */
-	signal(SIGINT, ShutDown);
+    /* Global shutdown handler */
+    signal(SIGINT, ShutDown);
 
-	xdp_detach(ifidx, XDP_FLAGS_DRV_MODE);
+    xdp_detach(ifidx, xdp_flags);
 
-	/* Load custom program if configured */
+    /* Load custom program if configured */
     struct bpf_map *map;
 
     bpf_obj = load_bpf_object_file__simple("src/afxdp_kern.o");
@@ -344,7 +345,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    if (xdp_attach(ifidx, XDP_FLAGS_DRV_MODE, progfd) != 0)
+    if (xdp_attach(ifidx, xdp_flags, progfd) != 0)
     {
         fprintf(stderr, "Error attaching XDP program: %s\n", strerror(errno));
 
@@ -363,22 +364,22 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-	if (setrlimit(RLIMIT_MEMLOCK, &rlim)) 
+    if (setrlimit(RLIMIT_MEMLOCK, &rlim)) 
     {
-		fprintf(stderr, "ERROR: setrlimit(RLIMIT_MEMLOCK) \"%s\"\n", strerror(errno));
+        fprintf(stderr, "ERROR: setrlimit(RLIMIT_MEMLOCK) \"%s\"\n", strerror(errno));
 
-		exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
-	/* Allocate memory for NUM_FRAMES of the default XDP frame size */
-	packet_buffer_size = NUM_FRAMES * FRAME_SIZE;
+    /* Allocate memory for NUM_FRAMES of the default XDP frame size */
+    packet_buffer_size = NUM_FRAMES * FRAME_SIZE;
 
-	if (posix_memalign(&packet_buffer, getpagesize(), packet_buffer_size)) 
+    if (posix_memalign(&packet_buffer, getpagesize(), packet_buffer_size)) 
     {
-		fprintf(stderr, "ERROR: Can't allocate buffer memory \"%s\"\n", strerror(errno));
+        fprintf(stderr, "ERROR: Can't allocate buffer memory \"%s\"\n", strerror(errno));
 
-		exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
     int cpus = 2;
 
@@ -421,8 +422,8 @@ int main(int argc, char **argv)
         sleep(1);
     }
 
-	/* Cleanup */
-	xdp_detach(ifidx, XDP_FLAGS_DRV_MODE);
+    /* Cleanup */
+    xdp_detach(ifidx, xdp_flags);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
